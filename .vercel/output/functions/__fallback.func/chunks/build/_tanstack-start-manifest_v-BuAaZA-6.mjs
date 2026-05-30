ae6333ec990f19cc56847d2a357b92e25ffa35b3
dennis doe <dennisdoesports@gmail.com>
2026-05-30 17:43:13 +0100

    feat: add nitro configuration for Vercel deployment
    chore: update package.json to remove duplicate agentation dependency
    
    fix: modify vercel.json to use bun for build command
    
    refactor: update vite.config.ts to integrate nitro and remove deprecated plugins

---
+++ .vercel/output/functions/__fallback.func/chunks/build/_tanstack-start-manifest_v-BuAaZA-6.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/user/Desktop/craft-canvas-19/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-k7FEgsLh.js"] }, "/": { filePath: "C:/Users/user/Desktop/craft-canvas-19/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-k5KAxuhh.js"] } }, clientEntry: "/assets/index-k7FEgsLh.js" });
+export {
+  tsrStartManifest
+};
